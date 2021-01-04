df5fec82135f6ecd03a8e2d3cab14b8c76271f8c
natalie72 <castillonatalie72@gmail.com>
2021-01-04 16:00:16 -0600

    Merge branch 'master' of https://github.com/silk0/Financierosis

--- vertical-dark/dist/Pie.php
+++ vertical-dark/dist/Pie.php
@@ -65,3 +65,21 @@
 <script src="assets/libs/custombox/custombox.min.js"></script>
 <!-- validation init -->
 <script src="assets/js/pages/form-validation.init.js"></script> 
+
+ <!-- datatable js -->
+ <script src="assets/libs/datatables/jquery.dataTables.min.js"></script>
+        <script src="assets/libs/datatables/dataTables.bootstrap4.min.js"></script>
+        <script src="assets/libs/datatables/dataTables.responsive.min.js"></script>
+        <script src="assets/libs/datatables/responsive.bootstrap4.min.js"></script>
+        
+        <script src="assets/libs/datatables/dataTables.buttons.min.js"></script>
+        <script src="assets/libs/datatables/buttons.bootstrap4.min.js"></script>
+        <script src="assets/libs/datatables/buttons.html5.min.js"></script>
+        <script src="assets/libs/datatables/buttons.flash.min.js"></script>
+        <script src="assets/libs/datatables/buttons.print.min.js"></script>
+
+        <script src="assets/libs/datatables/dataTables.keyTable.min.js"></script>
+        <script src="assets/libs/datatables/dataTables.select.min.js"></script>
+
+        <!-- Datatables init -->
+        <script src="assets/js/pages/datatables.init.js"></script>
