cf77cfae06334c8e2192fb9c6a86e0078d99228e
silk0 <silkfirmyn@gmail.com>
2021-01-04 00:17:35 -0600

    modal ver

--- vertical-dark/dist/listaCliente.php
+++ vertical-dark/dist/listaCliente.php
@@ -22,6 +22,35 @@ $id  = $_REQUEST["id"];
     }
 </script>
 
+<SCRIPT  language=JavaScript> 
+function go(){
+    //validacion respectiva me da hueva
+        document.form.submit();  
+} 
+function edit(id,nom,ape,dui,nit,prof,direc,tel,cel,email,tipo,sal,ob,egres)
+{
+
+    // document.getElementById("baccion2").value=id;
+    document.getElementById("nombrem").value=nom;
+    document.getElementById("apellidom").value=ape;
+    document.getElementById("duim").value=dui;
+    document.getElementById("nitm").value=nit;
+    document.getElementById("direcm").value=direc;
+    document.getElementById("telm").value=tel;
+    document.getElementById("celm").value=cel;
+    document.getElementById("emailm").value=email;    
+    document.getElementById("profecion").value=prof;
+    document.getElementById("tipom").value=tipo;
+    document.getElementById("salm").value=sal;
+    //document.getElementById("observm").value=ob;    
+    document.getElementById("egres").value=egres;
+    $("#custom-modal").modal();
+  }
+function modify(id){
+  document.location.href="editarCliente.php?id="+id;
+}
+</script>
+
 <body>
 
     <!-- Begin page -->
@@ -123,7 +152,21 @@ $id  = $_REQUEST["id"];
                                                     data-animation='contentscale' data-plugin='custommodal'
                                                     data-overlaySpeed='100'
                                                     data-overlayColor='#36404a'
-                                                    class='btn btn-primary waves-effect waves-light' onclick=\"edit('$fila->id_cliente','$fila->nombre','$fila->apellido','$fila->dui','$fila->nit','$fila->direccion','$fila->telefono','$fila->celular','$fila->correo','$fila->tipo_ingreso','$fila->profecion','$fila->salario','$fila->observaciones')\";>
+                                                    class='btn btn-primary waves-effect waves-light' onclick=\"
+                                                    edit('$fila->id_cliente',
+                                                    '$fila->nombre',
+                                                    '$fila->apellido',
+                                                    '$fila->dui',
+                                                    '$fila->nit',
+                                                    '$fila->profecion',
+                                                    '$fila->direccion',
+                                                    '$fila->telefono',
+                                                    '$fila->celular',
+                                                    '$fila->correo',
+                                                    '$fila->tipo_ingreso',                                                    
+                                                    '$fila->salario',
+                                                    '$fila->observaciones',
+                                                    '$fila->egreso')\";>
                                                         <i class='mdi mdi-eye'></i> 
                                                     </button>
                                                     <button class='btn btn-warning waves-effect waves-light' onclick='modify(" . $fila->id_cliente. ")'>
@@ -170,115 +213,83 @@ $id  = $_REQUEST["id"];
     <!-- END wrapper -->
     
     <!-- Custom Modal -->
-    <div id="custom-modal" class="modal-demo">
+    <div id="custom-modal" class="modal-demo containeR" aria-labelledby="myLargeModalLabel" >
+        <div class="container-flui">
         <button type="button" class="close" onclick="Custombox.modal.close();">
         <span>&times;</span><span class="sr-only">Close</span>
         </button>
-        <h4 class="custom-modal-title">Modificar datos del cliente</h4>
+        <h4 class="custom-modal-title">Datos del cliente</h4>
             <!-- Form row -->
             <div class="row">
                 <div class="col-md-12">
                     <div class="card-box">   
                         <form name="form" method="post" action="ingresoCliente.php?bandera=1" required class="parsley-examples">
-                        <input type="hidden" id="idfiador" name="idfiador">
+                        
                             <div class="form-row">
                                 <div class="form-group col-md-6">
                                     <label for="inputEmail4" class="col-form-label">Nombre</label>
-                                    <input type="text"  class="form-control" name="nombre" id="nombre" required  placeholder="Jose Alfredo">
+                                    <input type="text"  class="form-control" name="nombrem" id="nombrem" required  placeholder="Jose Alfredo" readonly>
                                 </div>
                                 <div class="form-group col-md-6">
                                     <label for="inputPassword4" class="col-form-label">Apellido</label>
-                                    <input type="text" class="form-control" name="apellido" id="apellido" required  placeholder="Rodriguez Perez">
+                                    <input type="text" class="form-control" name="apellidom" id="apellidom" required  placeholder="Rodriguez Perez" readonly>
                                 </div>
                             </div>
                             <div class="form-row">
                                 <div class="form-group col-md-6">
                                     <label for="inputEmail4" class="col-form-label">Dui</label>
-                                    <input type="text"  class="form-control" name="dui" id="dui" required  data-mask="99999999-9" placeholder="99999999-9">
+                                    <input type="text"  class="form-control" name="duim" id="duim" required  data-mask="99999999-9" placeholder="99999999-9" readonly>
                                 </div>
                                 <div class="form-group col-md-6">
                                     <label for="inputPassword4" class="col-form-label">Nit</label>
-                                    <input type="text" class="form-control" name="nit" id="nit" required  data-mask="9999-999999-999-9" placeholder="9999-999999-999-9">
+                                    <input type="text" class="form-control" name="nitm" id="nitm" required  data-mask="9999-999999-999-9" placeholder="9999-999999-999-9" readonly>
                                 </div>
                             </div>
                             <div class="form-row">
                                 <div class="form-group col-md-6">
                                     <label for="inputEmail4" class="col-form-label">Telefono fijo</label>
-                                    <input type="text"  class="form-control" name="telefono" id="telefono" required  data-mask="9999-9999" placeholder="9999-9999">
+                                    <input type="text"  class="form-control" name="telm" id="telm" required  data-mask="9999-9999" placeholder="9999-9999" readonly>
                                 </div>
                                 <div class="form-group col-md-6">
                                     <label for="inputPassword4" class="col-form-label">Telefono Movil</label>
-                                    <input type="text" class="form-control" name="celular" id="celular" required data-mask="9999-9999" placeholder="9999-9999">
+                                    <input type="text" class="form-control" name="celm" id="celm" required data-mask="9999-9999" placeholder="9999-9999" readonly>
                                 </div>
                             </div>
-                            <div class="form-group">
+                            <div class="form-row">
                                 <label for="inputAddress" class="col-form-label">Direccion</label>
-                                <input type="text" class="form-control"  name="direc" id="direc" required placeholder="Calle Juan Ulloa Canas y Avenida Crescencio Miranda Casa #23">
+                                <input type="text" class="form-control"  name="direcm" id="direcm" required placeholder="Calle Juan Ulloa Canas y Avenida Crescencio Miranda Casa #23" readonly>
                             </div>
                             <div class="form-row">
                                 <div class="form-group col-md-6">
                                     <label for="inputEmail4" class="col-form-label">Email</label>
-                                    <input type="email"  class="form-control" name="email" id="email"  required  placeholder="Correo@correo.com">
+                                    <input type="email"  class="form-control" name="emailm" id="emailm"  required  placeholder="Correo@correo.com" readonly>
                                 </div>
                                 <div class="form-group col-md-6">
                                     <label for="inputPassword4" class="col-form-label">Profesion u oficio</label>
-                                    <input type="text" class="form-control" name="profecion" id="profecion" required  placeholder="Rodriguez Perez">
+                                    <input type="text"  class="form-control" name="profecion" id="profecion" required  placeholder="Ing. Civil" readonly>
                                 </div>
                             </div>  
                             <div class="form-row">
                                 <div class="form-group col-md-4">
                                     <label for="inputCity" class="col-form-label">Ingresos Mensuales</label>
-                                    <input type="number" class="form-control" name="salario" id="salario" required  placeholder="0.00">
+                                    <input type="text" class="form-control" name="salm" id="salm" required  placeholder="0.00" readonly>
                                 </div>
                                 <div class="form-group col-md-4">
                                     <label for="inputState" class="col-form-label">Tipo de ingreso</label>
-                                    <select id="inputState" class="form-control" name="tipo" id="tipo" required >
-                                        <option selected >Seleccione</option>
-                                        <option>Salario</option>
-                                        <option>Remesa</option>
-                                        <option>Salario Informal</option>
-                                    </select>
+                                    <input type="text" class="form-control" name="tipom" id="tipom" placeholder="0.00" readonly>
                                 </div>
                                 <div class="form-group col-md-4">
                                     <label for="inputZip" class="col-form-label">Egreso Promedio Mensual</label>
-                                    <input type="number" class="form-control" name="egreso" id="egreso" placeholder="0.00" >
+                                    <input type="text" class="form-control" name="egres" id="egres" placeholder="0.00" readonly>
                                 </div>
                             </div>   
-                            <div class="form-row">                                        
-                                <div class="form-group col-md-4">
-                                    <label for="inputState" class="col-form-label">Agregar a la cartera</label>
-                                    <select id="inputState" class="form-control" name="cartera" id="cartera" required >
-                                        <option selected >Seleccione</option>
-                                        <?php
-                                        include 'config/conexion.php';
-                                        $result = $conexion->query("select id_categoria as id,nombre FROM tcartera");
-                                        if ($result) {
-                                            while ($fila = $result->fetch_object()) {
-                                                if ($fila->id == $idcaertera ) {
-                                                    echo '<option value="' . $fila->id. '" selected>' . $fila->nombre . '</opcion>';
-                                                }else {
-                                                    echo '<option value="' . $fila->id . '">' . $fila->nombre . '</opcion>';
-                                                }
-                                            }
-                                        }
-                                        ?> 
-                                    </select>
-                                </div>
-                            </div> 
-                            </br>
-                            <div class="form-row"> 
-                            <div class="form-group">
-                                <button type="buttom" onclick="go();" class="btn btn-success btn-rounded waves-light width-md">Modificar</button>                                        
-                                <button type="buttom" class="btn btn-danger btn-rounded waves-light width-md" onclick="Custombox.modal.close()">Cerrar</button>  
-                            </div>                                      
-                            </div>
+                            </br>                            
                         </form>
                     </div> <!-- end card-box -->
                 </div> <!-- end col -->
             </div>
             <!-- end row -->
-            
-        
+       </div>
     </div>                             
 
     <!-- Right bar overlay-->
