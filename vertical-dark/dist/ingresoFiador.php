aba940f356996e6a9e8558bff65b418ee6dee819
silk0 <silkfirmyn@gmail.com>
2021-01-06 17:33:49 -0600

    Reparacion de alqunos errores

--- vertical-dark/dist/ingresoFiador.php
+++ vertical-dark/dist/ingresoFiador.php
@@ -1,3 +1,16 @@
+<?php
+    //Codigo que muestra solo los errores exceptuando los notice.
+    error_reporting(E_ALL & ~E_NOTICE);
+    session_start();
+    if($_SESSION["logueado"] == TRUE) {
+    $usuario=$_SESSION["usuario"];
+    $nombre = $_SESSION["nombre"];
+    $tipo  = $_REQUEST["tipo"];
+    $id  = $_REQUEST["id"];
+    }else {
+        header("Location:../../../index.php");
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <?php include_once 'Cabecera.php';?>
@@ -5,40 +18,40 @@
 <script>
     function go(){
 
-//Validaciones
-if(document.getElementById('nombre').value==""){
-//    alert("El campo nombre es obligatorio");
-//    prueba :p
- notify(' Advertencia:','El campo Nombre es obligatorio.','top', 'right', 'any', 'warning');
-   document.getElementById("nombre").focus();
-}else if(document.getElementById('apellido').value==""){
-    notify(' Advertencia:','El campo Apellido es obligatorio,','top', 'right', 'any', 'warning');
-   document.getElementById("apellido").focus();
-}else if(document.getElementById('dui').value==""){
-    notify(' Advertencia:','El campo DUI es obligatorio','top', 'right', 'any', 'warning');
-   document.getElementById("dui").focus();
-}else if(document.getElementById('nit').value==""){
-    notify(' Advertencia:','El campo NIT es obligatorio', 'top', 'right', 'any', 'warning');
-   document.getElementById("nit").focus();
-}else if(document.getElementById('direc').value==""){
-    notify(' Advertencia:','El campo Direccion es obligatorio', 'top', 'right', 'any', 'warning');
-   document.getElementById("direc").focus();
-}else if(document.getElementById('telefono').value=="" && document.getElementById('celular').value==""){
-    notify(' Advertencia:','Ingrese telefono', 'top', 'right', 'any', 'warning');
-   document.getElementById("telefono").focus();
-}else if(document.getElementById('email').value==""){
-    notify(' Advertencia:','El campo E-mail es obligatorio,','top', 'right', 'any', 'warning');
-   document.getElementById("email").focus();
-}else if(document.getElementById('trabajo').value==""){
-    notify(' Advertencia:','El campo Tabajo que realiza es obligatorio', 'top', 'right', 'any', 'warning');
-   document.getElementById("trabajo").focus();
-}else if(document.getElementById('salario').value==""){
-    notify(' Advertencia:','El campo Salario  es obligatorio', 'top', 'right', 'any', 'warning');
-   document.getElementById("salario").focus();
-}else{
-  document.form.submit();  
-}   
-}
+    //Validaciones
+    if(document.getElementById('nombre').value==""){
+    //    alert("El campo nombre es obligatorio");
+    //    prueba :p
+        notify(' Advertencia:','El campo Nombre es obligatorio.','top', 'right', 'any', 'warning');
+        document.getElementById("nombre").focus();
+        }else if(document.getElementById('apellido').value==""){
+            notify(' Advertencia:','El campo Apellido es obligatorio,','top', 'right', 'any', 'warning');
+        document.getElementById("apellido").focus();
+        }else if(document.getElementById('dui').value==""){
+            notify(' Advertencia:','El campo DUI es obligatorio','top', 'right', 'any', 'warning');
+        document.getElementById("dui").focus();
+        }else if(document.getElementById('nit').value==""){
+            notify(' Advertencia:','El campo NIT es obligatorio', 'top', 'right', 'any', 'warning');
+        document.getElementById("nit").focus();
+        }else if(document.getElementById('direc').value==""){
+            notify(' Advertencia:','El campo Direccion es obligatorio', 'top', 'right', 'any', 'warning');
+        document.getElementById("direc").focus();
+        }else if(document.getElementById('telefono').value=="" && document.getElementById('celular').value==""){
+            notify(' Advertencia:','Ingrese telefono', 'top', 'right', 'any', 'warning');
+        document.getElementById("telefono").focus();
+        }else if(document.getElementById('email').value==""){
+            notify(' Advertencia:','El campo E-mail es obligatorio,','top', 'right', 'any', 'warning');
+        document.getElementById("email").focus();
+        }else if(document.getElementById('trabajo').value==""){
+            notify(' Advertencia:','El campo Tabajo que realiza es obligatorio', 'top', 'right', 'any', 'warning');
+        document.getElementById("trabajo").focus();
+        }else if(document.getElementById('salario').value==""){
+            notify(' Advertencia:','El campo Salario  es obligatorio', 'top', 'right', 'any', 'warning');
+        document.getElementById("salario").focus();
+        }else{
+        document.form.submit();  
+        }   
+    }
 </script>
 
 <body class="left-side-menu-dark">
@@ -195,7 +208,7 @@ if(document.getElementById('nombre').value==""){
 
 <?php
     include "config/conexion.php";
-    $accion = $_REQUEST['bandera'];
+        $accion = $_REQUEST['bandera'];
         if($accion==1){
         $nombre   = $_POST['nombre'];
         $apellido   = $_POST['apellido'];
