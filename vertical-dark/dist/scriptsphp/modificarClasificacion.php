9dc20062bc5101776a059bd366acc47508147baf
Natalie72 <castillonatalie72@gmail.com>
2021-01-15 15:26:31 -0600

    Clasificacion

--- vertical-dark/dist/scriptsphp/modificarClasificacion.php
+++ vertical-dark/dist/scriptsphp/modificarClasificacion.php
@@ -1,18 +1,20 @@
 <?php
 include "../config/conexion.php";
-$bandera = $_REQUEST['bandera'];
-
-if ($bandera==1) {
-$baccion  = $_REQUEST["idC"];
-$clasificacion  = $_REQUEST['clasificacion'];
+$accion = $_REQUEST['bandera'];
+$baccion  = $_REQUEST["baccion2"];
+if($accion==1){
+$id = $_POST["id_clasificacion"];
+$nombre   = $_POST['nombre'];
 $correlativo    = $_REQUEST['correlativo'];
-$depreciacion   = $_REQUEST['depreciacion'];
-    $consulta  = "UPDATE tclasificacion set nombre='" . $clasificacion . "',correlativo='" . $correlativo . "',tiempo_depreciacion='" . $depreciacion . "' where id_clasificaion='" . $baccion . "'";
+$depreciacion   = $_REQUEST['tiempo_depreciacion'];
+    $consulta  = "UPDATE tclasificacion set nombre='" . $nombre. "',correlativo='" . $correlativo . "',tiempo_depreciacion='" . $depreciacion . "' 
+    where id_clasificaion='" . $id . "'";
     $resultado = $conexion->query($consulta);
       if ($resultado) {
-        header('Location:../listaClasificacion.php?bandera=1');
+        header('Location:../clasificacionActivo.php?bandera=1');
       } else {
-        header('Location:../listaClasificacion.php?bandera=2');
+        header('Location:../clasificacionActivo.php?bandera=2');
       }
     }
     ?>
+
